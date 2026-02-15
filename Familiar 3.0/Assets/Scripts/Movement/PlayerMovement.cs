0c66201d3003b9ecc9d6690a67f84a12af90e3d1
Alexander Avteniev <aja2328@rit.edu>
2026-02-15 15:03:12 -0500

    Movement Script Started

---
+++ Familiar 3.0/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Fields
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float gravity;
+    [SerializeField] private GameObject player;
+
+    private Vector3 direction;
+    private Vector3 velocity;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+        
+    }
+
+}
