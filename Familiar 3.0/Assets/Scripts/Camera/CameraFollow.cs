68ef1785bafc5fb67e16aa9f0cb9acc8aed0354e
nmbdude <nmb7888@rit.edu>
2026-02-15 15:55:41 -0500

    Finished Camera Movement

--- Familiar 3.0/Assets/Scripts/Camera/CameraFollow.cs
+++ Familiar 3.0/Assets/Scripts/Camera/CameraFollow.cs
@@ -1,29 +1,17 @@
-using NUnit.Framework;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     //Serialized
-    [SerializeField, Header("Camera")]
-    private float followDistance = 10.0f;
-    [SerializeField]
-    private float followSpeed = 5.0f;
-    
-    //Private
-    private Camera cameraRef;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        cameraRef = GetComponentInChildren<Camera>();
-    }
+    [SerializeField, Header("Camera")] private float followDistance = 10.0f;
+    [SerializeField] private float followSpeed = 5.0f;
+    [SerializeField] private GameObject target;
+    [SerializeField] private float smoothSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        float distance =  Vector3.Distance(cameraRef.transform.position, transform.position);
-        Vector3 targetPos = cameraRef.transform.position + Vector3.up * followDistance;
-        cameraRef.transform.position = Vector3.Lerp(cameraRef.transform.position, targetPos, Time.deltaTime * followDistance * followSpeed);
-        
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x + followDistance, target.transform.position.y + followDistance,
+            target.transform.position.z), Time.deltaTime * smoothSpeed);
     }
 }
