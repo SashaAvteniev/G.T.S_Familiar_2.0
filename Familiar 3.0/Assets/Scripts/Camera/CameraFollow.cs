718813f1ecd3934d1b12dec5a552646a91413697
nmbdude <nmb7888@rit.edu>
2026-02-22 14:45:23 -0500

    Fixed Camera Jittering

--- Familiar 3.0/Assets/Scripts/Camera/CameraFollow.cs
+++ Familiar 3.0/Assets/Scripts/Camera/CameraFollow.cs
@@ -4,12 +4,11 @@ public class CameraFollow : MonoBehaviour
 {
     //Serialized
     [SerializeField, Header("Camera")] private float followDistance = 10.0f;
-    [SerializeField] private float followSpeed = 5.0f;
     [SerializeField] private GameObject target;
     [SerializeField] private float smoothSpeed;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x + followDistance, target.transform.position.y + followDistance,
             target.transform.position.z), Time.deltaTime * smoothSpeed);
