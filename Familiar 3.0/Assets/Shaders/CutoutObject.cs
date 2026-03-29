74f7d7ea2c12ba5dce17aa7df4d348628578bd32
nmbdude <nmb7888@rit.edu>
2026-03-29 15:59:48 -0400

    Updated the hole punch shader to greatly reduce snapping

--- Familiar 3.0/Assets/Shaders/CutoutObject.cs
+++ Familiar 3.0/Assets/Shaders/CutoutObject.cs
@@ -20,18 +20,50 @@ public class CutoutObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 offset = targetObject.position - transform.position;
+        // Center Raycast
+        RaycastHit[] centerRaycastHits = Physics.RaycastAll(transform.position, offset, offset.magnitude, wallMask);
+        Debug.DrawLine(transform.position, targetObject.position, Color.red);
+        UpdateMaterials(centerRaycastHits);
+        
+        // Left Raycast
+        RaycastHit[] leftRaycastHits = Physics.RaycastAll(transform.position, offset + new Vector3(0,0,-5), offset.magnitude, wallMask);
+        Debug.DrawRay(transform.position, offset + new Vector3(0,0,-5), Color.red);
+        UpdateMaterials(leftRaycastHits);
+        
+        // Right Raycast
+        RaycastHit[] rightRaycastHits = Physics.RaycastAll(transform.position, offset + new Vector3(0,0,5), offset.magnitude, wallMask);
+        Debug.DrawLine(transform.position, targetObject.position + new Vector3(0,0,5), Color.red);
+        UpdateMaterials(rightRaycastHits);
+        
+        // Upper Raycast
+        RaycastHit[] upperRaycastHits = Physics.RaycastAll(transform.position, offset + new Vector3(0,5,0), offset.magnitude, wallMask);
+        Debug.DrawLine(transform.position, targetObject.position + new Vector3(0,5,0), Color.red);
+        UpdateMaterials(upperRaycastHits);
+        
+        // Lower Raycast
+        RaycastHit[] lowerRaycastHits = Physics.RaycastAll(transform.position, offset + new Vector3(0,-5,0), offset.magnitude, wallMask);
+        Debug.DrawLine(transform.position, targetObject.position + new Vector3(0,-5,0), Color.red);
+        UpdateMaterials(lowerRaycastHits);
+    }
+
+    void UpdateMaterials(RaycastHit[] hits)
+    {
+        if (hits.Length == 0)
+        {
+            /*materials[m].SetVector("_CutoutPos", cutoutPos);
+            materials[m].SetFloat("_CutoutSize", cutoutSize);
+            materials[m].SetFloat("_FalloffSize", falloffSize);
+            materials[m].SetFloat("_ShowCutout", showCutout ? 1f : 0f);*/
+        }
         Vector2 cutoutPos = mainCamera.WorldToViewportPoint(targetObject.position);
         cutoutPos.y /= (Screen.width / Screen.height);
-        
-        Vector3 offset = targetObject.position - transform.position;
-        RaycastHit[] hitObjects = Physics.RaycastAll(transform.position, offset, offset.magnitude, wallMask);
-
-        for (int i = 0; i < hitObjects.Length; i++)
+        for (int i = 0; i < hits.Length; i++)
         {
-            Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
+            Material[] materials = hits[i].transform.GetComponent<Renderer>().materials;
             for (int m = 0; m < materials.Length; ++m)
             {
-                Debug.Log($"Setting cutout for {hitObjects[i].transform.name} material {m}");
+                //Debug.Log($"Setting cutout for {hits[i].transform.name} material {m}");
                 materials[m].SetVector("_CutoutPos", cutoutPos);
                 materials[m].SetFloat("_CutoutSize", cutoutSize);
                 materials[m].SetFloat("_FalloffSize", falloffSize);
