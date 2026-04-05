3c4121cd4c3754fa4c2e0428373d56c11b3de6a7
nmbdude <nmb7888@rit.edu>
2026-04-05 15:50:43 -0400

    Worked on holepunch shader bug

--- Familiar 3.0/Assets/Shaders/CutoutObject.cs
+++ Familiar 3.0/Assets/Shaders/CutoutObject.cs
@@ -7,8 +7,7 @@ public class CutoutObject : MonoBehaviour
     [SerializeField] private float falloffSize = 0.05f;
     [SerializeField] private Transform targetObject;
     [SerializeField] private LayerMask wallMask;
-    
-    [SerializeField] private float fadeInDuration = 0.5f;
+    public Vector3 test;
     
     private Camera mainCamera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,27 +22,32 @@ public class CutoutObject : MonoBehaviour
         Vector3 offset = targetObject.position - transform.position;
         // Center Raycast
         RaycastHit[] centerRaycastHits = Physics.RaycastAll(transform.position, offset, offset.magnitude, wallMask);
-        Debug.DrawLine(transform.position, targetObject.position, Color.red);
+        Debug.DrawLine(transform.position, offset, Color.red);
+        Debug.Log("Center: " + centerRaycastHits.Length);
         UpdateMaterials(centerRaycastHits);
         
         // Left Raycast
         RaycastHit[] leftRaycastHits = Physics.RaycastAll(transform.position, offset + new Vector3(0,0,-5), offset.magnitude, wallMask);
-        Debug.DrawRay(transform.position, offset + new Vector3(0,0,-5), Color.red);
+        Debug.DrawRay(transform.position, offset + new Vector3(0,0,-5), Color.blue);
+        Debug.Log("Left: " + leftRaycastHits.Length);
         UpdateMaterials(leftRaycastHits);
         
         // Right Raycast
         RaycastHit[] rightRaycastHits = Physics.RaycastAll(transform.position, offset + new Vector3(0,0,5), offset.magnitude, wallMask);
-        Debug.DrawLine(transform.position, targetObject.position + new Vector3(0,0,5), Color.red);
+        Debug.DrawLine(transform.position, offset + new Vector3(0,0,5), Color.green);
+        Debug.Log("Right: " + rightRaycastHits.Length);
         UpdateMaterials(rightRaycastHits);
         
         // Upper Raycast
         RaycastHit[] upperRaycastHits = Physics.RaycastAll(transform.position, offset + new Vector3(0,5,0), offset.magnitude, wallMask);
-        Debug.DrawLine(transform.position, targetObject.position + new Vector3(0,5,0), Color.red);
+        Debug.DrawLine(transform.position, offset + new Vector3(0,5,0), Color.purple);
+        Debug.Log("Upper: " + upperRaycastHits.Length);
         UpdateMaterials(upperRaycastHits);
         
         // Lower Raycast
-        RaycastHit[] lowerRaycastHits = Physics.RaycastAll(transform.position, offset + new Vector3(0,-5,0), offset.magnitude, wallMask);
-        Debug.DrawLine(transform.position, targetObject.position + new Vector3(0,-5,0), Color.red);
+        RaycastHit[] lowerRaycastHits = Physics.RaycastAll(transform.position, offset + test, offset.magnitude, wallMask);
+        Debug.DrawLine(transform.position, offset + test, Color.orange);
+        Debug.Log("Lower: " + lowerRaycastHits.Length);
         UpdateMaterials(lowerRaycastHits);
     }
 
@@ -51,10 +55,8 @@ public class CutoutObject : MonoBehaviour
     {
         if (hits.Length == 0)
         {
-            /*materials[m].SetVector("_CutoutPos", cutoutPos);
-            materials[m].SetFloat("_CutoutSize", cutoutSize);
-            materials[m].SetFloat("_FalloffSize", falloffSize);
-            materials[m].SetFloat("_ShowCutout", showCutout ? 1f : 0f);*/
+            Debug.Log("No Hits");
+            Shader.SetGlobalFloat("ShowCutout", 0f);
         }
         Vector2 cutoutPos = mainCamera.WorldToViewportPoint(targetObject.position);
         cutoutPos.y /= (Screen.width / Screen.height);
