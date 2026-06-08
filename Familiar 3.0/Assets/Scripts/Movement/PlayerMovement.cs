157b35ef641cef714ad7f678ce8c24f3b22fcfab
Alexander Avteniev <aja2328@rit.edu>
2026-06-08 12:33:36 -0400

    Jumping and custom collisions set up

--- Familiar 3.0/Assets/Scripts/Movement/PlayerMovement.cs
+++ Familiar 3.0/Assets/Scripts/Movement/PlayerMovement.cs
@@ -7,10 +7,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float movementSpeed;
     [SerializeField] private float gravity;
     [SerializeField] private GameObject player;
-    [SerializeField] private bool stag;
+ 
 
     private Vector3 direction;
-    private Vector3 velocity;
+    private Vector3 velocityHorizontal;
+    private Vector3 velocityVertical;
 
     //Jumping
     [SerializeField] private float jumpHeight;
@@ -18,36 +19,30 @@ public class PlayerMovement : MonoBehaviour
     private bool holdingJump;
     private bool grounded;
 
-    private Talisman currentActive;
-    enum Talisman
-    {
-        Elk,
-        Badger,
-        Sheep,
-        Snake
-    }
+    //Collisions
+    private Vector3 currentWallNormal;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         grounded = true;
+        velocityVertical = Vector3.zero;
+        velocityHorizontal = Vector3.zero;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         #region calculate velocity
-        velocity = new Vector3(direction.x*movementSpeed, direction.y, direction.z*movementSpeed);
+        velocityHorizontal = new Vector3(direction.x*movementSpeed, 0, direction.z*movementSpeed);
         ApplyGravity();
+        HandleWallCollision();
         #endregion
         #region apply velocity
-        player.transform.position = player.transform.position + (direction * movementSpeed * Time.deltaTime);
+        player.transform.position = player.transform.position + velocityHorizontal * Time.deltaTime + velocityVertical*Time.deltaTime;
         #endregion
 
-        if (stag)
-        {
-            currentActive = Talisman.Elk;
-        }
+
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -60,12 +55,10 @@ public class PlayerMovement : MonoBehaviour
     #region Jumping
     public void Jump(InputAction.CallbackContext context)
     {
-        if(context.started)
+        if(context.started && grounded)
         {
-            if(currentActive == Talisman.Elk)
-            {
-
-            }
+            velocityVertical.y = Vector3.up.y * jumpHeight;
+            grounded = false;
         }
     }
     
@@ -73,7 +66,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if(!grounded)
         {
-            direction.y -= gravity * Time.deltaTime;
+            velocityVertical.y -= gravity * Time.deltaTime;
         }
     }
     #endregion
@@ -82,8 +75,49 @@ public class PlayerMovement : MonoBehaviour
         if(collision.gameObject.layer == LayerMask.NameToLayer("floor"))
         {
             grounded = true;
-            direction.y = 0;
+            velocityVertical.y = 0;
             Debug.Log("hit");
         }
+        foreach(ContactPoint contactPoint in collision.contacts)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Walls"))
+            {
+                currentWallNormal = contactPoint.normal;
+                Debug.Log(currentWallNormal);
+            }
+            
+        }
+
+    }
+
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Walls"))
+        {
+            currentWallNormal = Vector3.zero;
+        }
+    }
+
+    private void HandleWallCollision()
+    {
+        if(velocityHorizontal.x > 0)
+        {
+            velocityHorizontal.x = velocityHorizontal.x - (currentWallNormal.x * -velocityHorizontal.x);
+        }
+        if (velocityHorizontal.x < 0)
+        {
+            velocityHorizontal.x = velocityHorizontal.x - (currentWallNormal.x * velocityHorizontal.x);
+        }
+        if (velocityHorizontal.z > 0)
+        {
+            velocityHorizontal.z = velocityHorizontal.z - (currentWallNormal.z * -velocityHorizontal.z);
+        }
+        if (velocityHorizontal.z < 0)
+        {
+            velocityHorizontal.z = velocityHorizontal.z - (currentWallNormal.z * velocityHorizontal.z);
+        }
+
     }
 }
