3c877e4bdd41b6c4932ad1d52ab7d1c6db91f8e4
AlwynCD <cac4172@rit.edu>
2026-06-18 22:46:40 -0500

    Pause Menu
    --in progress--
    
    The visual elements have all been set up and most of the functionality such as going in between pages
    
    Currently working on having the game freeze when a menu is up, and having the pause menu come up when you press 'esc'
    
    --bug 1--
    Currently, the if statement to have the pause menu appear doesn't work. I think the code thinks paused is false and true at the same time?
    
    the if statement will only run if I change paused from !paused --> paused, but for some reason when I do that, the freeze still works normally which I don't think it should.
    
    My best guess so far is that the close window button on the welcome page is temp updating the paused var, but I also don't think this is right.
    
    --bug 2--
    When the pause menu does come up, if you go into control and leave it brings up the welcome page again which it shouldn't. If you the pause menu individually it works perfectly fine.

---
+++ Familiar 3.0/Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class MenuUI : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool paused;
+
+    void Start()
+    {
+        //temp since playtester info at start
+        SetPaused(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for some reason paused ends up as true after I close the welcome page
+        //the player is still allowed to move even though it thinks paused = true
+        //the only way to get this if to work is if it's "&& paused"
+        //but when it works you go to control, and then go back to pause menu all the panels appear
+        if (Keyboard.current.escapeKey.wasPressedThisFrame && !paused)
+        {
+            Debug.Log("if paused = " + paused);
+            SetPaused(true);
+            pauseMenu.SetActive(true);
+        }
+    }
+
+    public void SetPaused(bool value)
+    {
+        Debug.Log("before set val paused = " + paused);
+        paused = value;
+        Debug.Log("after set val paused = " + paused);
+
+        // //if pause == true, timeScale = 0.0f (frozen)
+        // //if paused == false, timeScale = 1.0f (normal speed)
+        Time.timeScale = paused ? 0.0f : 1.0f;
+    }
+}
