b72ad17e5a98b6758e20373420e1b2571663f0a6
nmbdude <nmb7888@rit.edu>
2026-06-19 16:43:40 -0500

    Redid the UI architechture and handled easy future proofing for nested UI screens.

--- Familiar 3.0/Assets/Scripts/UI/MenuUI.cs
+++ Familiar 3.0/Assets/Scripts/UI/MenuUI.cs
@@ -1,42 +1,71 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using System.Collections;
+using UnityEditor;
+using System.Net.NetworkInformation;
 
 public class MenuUI : MonoBehaviour
 {
     public GameObject pauseMenu;
     public bool paused;
+    public GameObject startingUI;
+
+    private Stack menuOrder = new Stack();
 
     void Start()
     {
-        //temp since playtester info at start
-        SetPaused(true);
+        paused = true;
+        Time.timeScale = 0.0f;
+        menuOrder.Push(startingUI);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(paused);
-        //for some reason paused ends up as true after I close the welcome page
-        //the player is still allowed to move even though it thinks paused = true
-        //the only way to get this if to work is if it's "&& paused"
-        //but when it works you go to control, and then go back to pause menu all the panels appear
         if (Keyboard.current.escapeKey.wasPressedThisFrame && !paused)
         {
-            Debug.Log("if paused = " + paused);
-            SetPaused(true);
-            pauseMenu.SetActive(true);
+            SetPause(true);
         }
     }
 
-    public void SetPaused(bool value)
+    public void SetPause(bool value)
     {
-        //Debug.Log("before set val paused = " + paused);
-        paused = value;
-        //Debug.Log("after set val paused = " + paused);
+        if(value)
+        {
+            //Freeze time
+            Time.timeScale = 0.0f;
+            paused = value;
+            pauseMenu.SetActive(true);
+            menuOrder.Push(pauseMenu.gameObject);
+            return;
+        }
 
-        // //if pause == true, timeScale = 0.0f (frozen)
-        // //if paused == false, timeScale = 1.0f (normal speed)
-        Time.timeScale = paused ? 0.0f : 1.0f;
+        paused = value;
+        menuOrder.Clear();
+        Time.timeScale = 1.0f;
+    }
+
+
+    public void GoForward(GameObject nextUI)
+    {
+        GameObject top = (GameObject)menuOrder.Peek();
+        top.SetActive(false);
+        menuOrder.Push(nextUI);
+        nextUI.SetActive(true);
+    }
+
+    public void GoBack()
+    {
+        GameObject top = (GameObject)menuOrder.Pop();
+        top.SetActive(false);
+        top = (GameObject)menuOrder.Peek();
+        top.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("THE GAME HAS QUIT");
+        Application.Quit();
     }
 }
