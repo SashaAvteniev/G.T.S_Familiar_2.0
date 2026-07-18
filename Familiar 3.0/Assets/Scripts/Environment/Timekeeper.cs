d648982ae9a0eecfa97a3837f289d49b529f9a34
nmbdude <nmb7888@rit.edu>
2026-07-18 10:51:52 -0500

    Finished the timekeeper, lights now update with time of day

--- Familiar 3.0/Assets/Scripts/Environment/Timekeeper.cs
+++ Familiar 3.0/Assets/Scripts/Environment/Timekeeper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.PackageManager;
 using UnityEngine;
@@ -15,59 +16,98 @@ public class Timekeeper : MonoBehaviour
 
     [Tooltip("Day & night length (seperatley) in minutes. E.g. a value of 5 would be 5 minutes of daylight, and 5 minutes of nightime.")]
     public float dayLength = 10f;
-    private float seconds = 0f;
-    private float minuteLength = 0;
     private float fullDaySeconds = 0f;
     private Light sun;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sun = directionalLight.GetComponent<Light>();
+        fullDaySeconds = dayLength * 60f;
     }
 
+    // Allows us to see all lighting changes in the editor so we don't need to press play
     private void OnValidate()
     {
+        if(!simulateTime) { return; }
         float sunRotation = Mathf.Lerp(-90, 270, currentTime/2400f);
+        sun = directionalLight.GetComponent<Light>();
+
+        // Actually rotating the sun gmae object
         directionalLight.transform.SetPositionAndRotation(new Vector3(), Quaternion.Euler(sunRotation, directionalLight.transform.rotation.y, directionalLight.transform.rotation.z));
-        fullDaySeconds = dayLength * 60f;
+        
+        // Fades sun intensity in/out as it rises and sets 
+        if(currentTime <= 1200f){ sun.intensity = Mathf.InverseLerp(600f, 1200f, currentTime); }
+        else { sun.intensity = 1f - Mathf.InverseLerp(1200f, 1800f, currentTime); }
+
+        // Handle lights turning on and off with day & night
+
+        //Get all exterior lights
+        GameObject[] extLights = GameObject.FindGameObjectsWithTag("ExtLight");
+        for(int i = 0; i < extLights.Length; i++)
+        {
+            Light l;
+            if (l = extLights[i].GetComponentInChildren<Light>()) 
+            {
+                if(currentTime <= 1200f){ l.intensity = 1f- Mathf.InverseLerp(600f, 1200f, currentTime); }
+                else { l.intensity = Mathf.InverseLerp(1200f, 1800f, currentTime); }
+            }
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         if(!simulateTime) { return; }
+
+        // Converts real time to game time
         currentTime += 2400f / fullDaySeconds * Time.deltaTime;
-        if(currentTime >= 2400) {currentTime = 0;}
+        if(currentTime >= 2400) {currentTime = 0;} // Restart the day
         float sunRotation = Mathf.Lerp(-90, 270, currentTime/2400f);
         directionalLight.transform.SetPositionAndRotation(new Vector3(), Quaternion.Euler(sunRotation, directionalLight.transform.rotation.y, directionalLight.transform.rotation.z));
-        if(currentTime <= 1200f)
+
+        // Fades sun intensity in/out as it rises and sets 
+        if(currentTime <= 1200f) { sun.intensity = Mathf.InverseLerp(600f, 1200f, currentTime); } 
+        else {sun.intensity = 1f - Mathf.InverseLerp(1200f, 1800f, currentTime);}
+
+                //Get all exterior lights
+        GameObject[] extLights = GameObject.FindGameObjectsWithTag("ExtLight");
+        for(int i = 0; i < extLights.Length; i++)
         {
-            sun.intensity = Mathf.InverseLerp(600f, 1200f, currentTime);
-        } else {sun.intensity = 1f - Mathf.InverseLerp(1200f, 1800f, currentTime);}
-        //Debug.Log(sun.intensity);
+            Light l;
+            if (l = extLights[i].GetComponent<Light>()) 
+            {
+                if(currentTime <= 1200f){ l.intensity = 1f - Mathf.InverseLerp(600f, 1200f, currentTime); }
+                else { l.intensity = Mathf.InverseLerp(1200f, 1800f, currentTime); }
+            }
+
+        }
     }
 
 
     // Returns the current hour (0-23)
-    float GetHour()
+    int GetHour()
     {
         string time = currentTime.ToString();
         switch(time.Length)
         {
-            case 1:
-                return 0;
-            case 3:
-                {
-                    return time[0];
-                }
-            case 4:
-                {
-                    return time[0] + time[1];
-                }
-            default:
-                {
-                    return -1;
-                }
+            case 1: return 0;
+            case 3: return time[0];
+            case 4: return time[0] + time[1];
+            default: return -1;
+        }
+    }
+
+    int GetMinute()
+    {
+        string time = currentTime.ToString();
+        switch(time.Length)
+        {
+            case 1: return 0;
+            case 2: return (int)currentTime;
+            case 3: return time[1] + time[2];
+            case 4: return time[2] + time[3];
+            default: return -2;
         }
     }
 }
