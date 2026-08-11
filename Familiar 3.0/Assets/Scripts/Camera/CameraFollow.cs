f6d1b1ac867efa31c4977ea8423d23e7753025cf
nmbdude <nmb7888@rit.edu>
2026-08-11 16:47:08 -0500

    Lots of files renamed. New puzzle scene. Needs piano UI done.

--- Familiar 3.0/Assets/Scripts/Camera/CameraFollow.cs
+++ Familiar 3.0/Assets/Scripts/Camera/CameraFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -6,13 +7,24 @@ public class CameraFollow : MonoBehaviour
     [SerializeField, Header("Camera")] private float followDistance = 10.0f;
     [SerializeField] private GameObject target;
     [SerializeField] private float smoothSpeed;
+    [SerializeField] private Vector3 offset;
 
     public float SmoothSpeed { get { return smoothSpeed; } set { smoothSpeed = value; } }
+
+    private void OnValidate()
+    {
+        if (!target) return;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x + followDistance + 1 + offset.x,
+            target.transform.position.y + followDistance * 0.75f + offset.y,
+            target.transform.position.z + offset.z), Time.deltaTime * smoothSpeed);
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x + followDistance + 1, target.transform.position.y + followDistance * 0.75f,
-            target.transform.position.z), Time.deltaTime * smoothSpeed);
+        if (!target) return;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x + followDistance + 1 + offset.x,
+            target.transform.position.y + followDistance * 0.75f + offset.y,
+            target.transform.position.z + offset.z), Time.deltaTime * smoothSpeed);
     }
 }
